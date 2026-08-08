8ef3ea2cafb5dc577765e9a625d61841596fb233
abrarhasmani04-beep <abrarhasmani04@gmail.com>
2026-08-08 16:46:51 +0530

    ratelimit fix

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -8,11 +8,11 @@ import authRateLimiter from '../middlewares/authRateLimiter.js'
 const authRoute = express.Router()
 
 authRoute.post('/user/register',registerValidation,validationMiddleware,registerUser)
-authRoute.post('/user/login',authRateLimiter,loginUser)
+authRoute.post('/user/login',loginUser)
 authRoute.post('/user/verify-email',verifyEmail)
 authRoute.post('/user/login-with-otp',loginWithOTP)
-authRoute.post('/user/verify-login-otp',authRateLimiter,verifyLoginOTP)
-authRoute.post('/user/forgot-password',authRateLimiter,forgotPassword)
+authRoute.post('/user/verify-login-otp',verifyLoginOTP)
+authRoute.post('/user/forgot-password',forgotPassword)
 authRoute.post('/user/reset-password',resetPassword)
 authRoute.post('/user/logout',logoutUser)
 authRoute.get("/user/me", authMiddleware, getCurrentUser);
